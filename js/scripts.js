1fef711ca40b47bafd8068284f38e54cba3f4281
koonradd <35108423+koonradd@users.noreply.github.com>
2018-02-03 23:18:09 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
+var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 
 function getQuote() {
-    $.getJSON(quoteUrl, createTweet);
+    $.getJSON(prefix + quoteUrl, createTweet);
+    $.ajaxSetup({ cache: false });
 }
 
 function createTweet(input) {
@@ -15,12 +17,9 @@ function createTweet(input) {
     if (!quoteAuthor.length) {
         quoteAuthor = "Unknown author";
     }
-}
-
-var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
-
-
-if (tweetText.length > 140) {
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+    
+    if (tweetText.length > 140) {
     getQuote();
 } else {
     var tweet = tweetLink + encodeURIComponent(tweetText);
@@ -28,6 +27,10 @@ if (tweetText.length > 140) {
     $('.author').text("Author: " + quoteAuthor);
     $('.tweet').attr('href', tweet);
 }
+
+}
+
+
 
 $(document).ready(function() {
     getQuote();
